116aa5051a24241a30b6a55fb5fa865bc53af59b
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-27 12:05:31 +0100

    create comments; add publish method; export the Pubsub function

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,14 +1,22 @@
-export default (function pubsub() {
-  const _event = {};
+export default (function Pubsub() {
+  const _events = {};
 
-  const subscribe = (eName, func) => {
-    // if eventName does not exist, create event and assign an array
-    if (!_event[eName]) _event[eName] = [];
-    _event[eName].push(func);
-    console.log(_event);
+  const subscribe = (evName, func) => {
+    // assign it to itself only if it exists, or create empty []
+    _events[evName] = _events[evName] || [];
+    _events[evName].push(func);
+    console.log(`PUBSUB: ${func} just subscribed to ${evName}`);
+  };
+
+  const publish = (evName, data) => {
+    if (_events[evName]) {
+      _events[evName].forEach((func) => func(data));
+    }
+    console.log(`PUBSUB: Making a broadcast about ${evName} with ${data}`);
   };
 
   return {
     subscribe,
+    publish,
   };
 })();
