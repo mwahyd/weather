071133045ce49f16c73b4c4d0568446a9887c467
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-28 15:25:50 +0100

    update URL

--- src/async.js
+++ src/async.js
@@ -13,13 +13,12 @@ export function listen() {
 function _getData(location) {
   console.log(location);
 
-  const URL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`;
+  const URL = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=1`;
 
   // attempt to use location provided to fetch data,
   fetch(URL)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       // if failed ... publish error
       if (data["error"]) {
         Pubsub.publish("locationStatus", "error");
