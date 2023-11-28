a2a9d98f718618d631c0ac22c8444d80dbabd19b
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-28 11:50:35 +0100

    get default locaion data on page load; get elements for data transposition

--- src/DOM.js
+++ src/DOM.js
@@ -4,6 +4,7 @@ export default function DOM() {
   //listeners
   const _listeners = () => {
     Pubsub.subscribe("locationStatus", _locationStatus);
+    Pubsub.subscribe("locationData", _updateDOM);
   };
 
   // cache DOM
@@ -11,6 +12,9 @@ export default function DOM() {
 
   // bind events
   const getLocation = () => {
+    // initial location
+    document.addEventListener("DOMContentLoaded", _getDefaultLocation);
+
     const searchBtn = doc.querySelector("[data-search-btn]");
     searchBtn.addEventListener("click", _getInput);
   };
@@ -26,12 +30,36 @@ export default function DOM() {
     searchInput.value = "";
   };
 
+  const _getDefaultLocation = () => {
+    Pubsub.publish("locationEntered", "london");
+  };
+
   // support functions
   const _locationStatus = (status) => {
     const errorDiv = doc.querySelector("[data-error]");
     status === "error"
-      ? (errorDiv.textContent = `\u2022 Location Error`)
+      ? (errorDiv.textContent = `\u2022 Location Error!`)
       : (errorDiv.textContent = "");
+  };
+
+  const _updateDOM = (data) => {
+    console.log(data);
+    const location = doc.querySelector("#location");
+
+    // current object
+    const temp = doc.querySelector("#temp");
+    const rFeel = doc.querySelector("#rfeel");
+    const humi = doc.querySelector("#humi");
+    const windSp = doc.querySelector("#wind-sp");
+
+    // forecast object
+    const tempH = doc.querySelector("#high");
+    const tempL = doc.querySelector("#low");
+    const rainProb = doc.querySelector("#chance-rain");
+  };
+
+  const _getCondition = () => {
+    // {light_rain: icon}
   };
 
   // internal calls
