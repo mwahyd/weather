adaf02f42209e2bc84cc59e15fe97de195ffbd17
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-29 15:34:21 +0100

    create _setForecastHourly(); based on the button clicked, display daily or hourly forecasts

--- src/DOM.js
+++ src/DOM.js
@@ -11,13 +11,20 @@ export default function DOM() {
   const doc = document.querySelector("[data-container]");
 
   // bind events
-  const getLocation = () => {
+  const bindEvents = () => {
     // initial location
     document.addEventListener("DOMContentLoaded", _getDefaultLocation);
 
     const searchBtn = doc.querySelector("[data-search-btn]");
     searchBtn.addEventListener("click", _getInput);
     window.addEventListener("keydown", _registerEnter);
+
+    // forecast display
+    const dailyBtn = doc.querySelector("[data-daily]");
+    const hourlyBtn = doc.querySelector("[data-hourly]");
+
+    hourlyBtn.addEventListener("click", _handleHourlyForecast);
+    dailyBtn.addEventListener("click", _handleDailyForecast);
   };
 
   // handler functions
@@ -39,6 +46,20 @@ export default function DOM() {
     Pubsub.publish("locationEntered", "london");
   };
 
+  const _handleHourlyForecast = (event) => {
+    const daily = doc.querySelectorAll(".group.daily");
+    const hourly = doc.querySelectorAll(".group.hourly");
+    daily.forEach((item) => item.classList.add("hide"));
+    hourly.forEach((item) => item.classList.remove("hide"));
+  };
+
+  const _handleDailyForecast = (event) => {
+    const daily = doc.querySelectorAll(".group.daily");
+    const hourly = doc.querySelectorAll(".group.hourly");
+    daily.forEach((item) => item.classList.remove("hide"));
+    hourly.forEach((item) => item.classList.add("hide"));
+  };
+
   // support functions
   const _locationStatus = (status) => {
     const errorDiv = doc.querySelector("[data-error]");
@@ -53,6 +74,7 @@ export default function DOM() {
     _setLocationConditions(data);
     _setForecastConditions(data);
     _setForecastDays(data);
+    _setForecastHourly(data);
   };
 
   const _setBackground = (data) => {
@@ -155,8 +177,8 @@ export default function DOM() {
       const options = { weekday: "long" };
       const dayWeek = date.toLocaleDateString("en-GB", options);
 
-      console.log(array);
-      console.log(dayWeek);
+      // console.log(array);
+      // console.log(dayWeek);
 
       const group = document.createElement("div");
       const day = document.createElement("p");
@@ -171,8 +193,37 @@ export default function DOM() {
       condition.classList.add("forecast-img");
 
       day.classList.add("day");
-      group.classList.add("group");
+      group.classList.add("group", "daily");
       group.append(day, tempH, tempL, condition);
+      forecast.appendChild(group);
+    });
+  };
+
+  const _setForecastHourly = (data) => {
+    const forecast = doc.querySelector("[data-forecast]");
+    console.log(data["forecast"]["forecastday"][0]["hour"]);
+    data["forecast"]["forecastday"][0]["hour"].forEach((segment) => {
+      const forecastTime = new Date(segment["time"]);
+      const options = { hour: "numeric", minute: "numeric" };
+      const time = forecastTime.toLocaleTimeString("en-GB", options);
+
+      console.log(segment["time"]);
+      console.log(time);
+
+      const group = document.createElement("div");
+      const hour = document.createElement("p");
+      const tempAvg = document.createElement("p");
+      const condition = document.createElement("img");
+
+      hour.textContent = time;
+      tempAvg.textContent = `${Math.floor(segment["temp_c"])}°`;
+      condition.src = segment["condition"]["icon"];
+      condition.classList.add("forecast-img");
+
+      hour.classList.add("day");
+      group.classList.add("group", "hourly", "hide");
+
+      group.append(hour, tempAvg, condition);
       forecast.appendChild(group);
     });
   };
@@ -181,6 +232,6 @@ export default function DOM() {
   _listeners();
 
   return {
-    getLocation,
+    bindEvents,
   };
 }
