5d60d8bb8a47e04df70cec5477a80bdfc4ff5093
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-30 16:47:42 +0100

    import css and images

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+import style from "./style.css";
+import bgDay from "./assets/backgrounds/day_hill.gif";
+import bgNight from "./assets/backgrounds/night_hill.gif";
+import fogIco from "./assets/icons/fog.png";
+import humiIco from "./assets/icons/humi-icon.gif";
+import rainIco from "./assets/icons/rain-icon.gif";
+import rfIco from "./assets/icons/rf-icon.gif";
+import sunriseIco from "./assets/icons/sunrise-icon.gif";
+import sunsetIco from "./assets/icons/sunset-icon.gif";
+import windIco from "./assets/icons/wind-icon.gif";
+
 import DOM from "./DOM.js";
 import { listen as async_listen } from "./async.js";
 
