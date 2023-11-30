3781e9feaf78159f638190243b7c606816de3db6
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-30 17:40:02 +0100

    fix typo

--- src/async.js
+++ src/async.js
@@ -7,7 +7,7 @@ export function listen() {
 // support functions
 function _getData(location) {
   const key = "2435aa0ce3b14dfc9c7163807232711";
-  const URL = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=5`;
+  const URL = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=5`;
 
   // attempt to use location provided to fetch data,
   fetch(URL)
