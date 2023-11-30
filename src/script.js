e5404f4bb036ccec923ab484fd7534a34636bb55
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-30 15:26:14 +0100

    call init function to set up project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-// import css
+import DOM from "./DOM.js";
+import { listen as async_listen } from "./async.js";
+
+const listeners = {
+  init: () => {
+    // set up listener for async.js
+    async_listen();
+
+    // listen to user input
+    DOM().bindEvents();
+  },
+};
+
+listeners.init();
